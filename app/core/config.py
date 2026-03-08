ffc1a21dc601a713e067a026d31b5fe9a8cd2aec
ralapont <rafael.alapont.gomez@gmail.com>
2026-03-08 11:53:19 +0100

    documenta la conexión a base de datos

--- app/core/config.py
+++ app/core/config.py
@@ -3,6 +3,14 @@ from pydantic import field_validator
 
 
 class Settings(BaseSettings):
+    """
+    Clase de configuración de la aplicación.
+
+    Atributos:
+        database_url (str): URL de conexión a la base de datos.
+        debug (bool): Indica si la aplicación corre en modo debug.
+        secret_key (str): Clave secreta utilizada para la seguridad.
+    """
     APP_ENV: str = "local"
     APP_NAME: str = "auth-service"
 
@@ -23,6 +31,24 @@ class Settings(BaseSettings):
     @field_validator("DATABASE_URL", mode="before")
     @classmethod
     def build_url(cls, v, info):
+        """
+        Genera automáticamente la cadena de conexión a la base de datos si el
+        usuario no ha proporcionado explícitamente el valor de `DATABASE_URL`.
+
+        Este validador se ejecuta **antes** de asignar el valor al campo
+        `DATABASE_URL`. Si el valor (`v`) existe, simplemente se devuelve tal cual.
+        Si no existe, el método construye la URL usando otros campos ya validados
+        en el modelo (`DB_USER`, `DB_PASSWORD`, `DB_HOST`, `DB_PORT`, `DB_NAME`)
+        accediendo a ellos a través de `info.data`.
+
+        La URL generada utiliza el driver asincrónico `aiomysql` y sigue el
+        formato:
+
+            mysql+aiomysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}
+
+        Returns:
+            str: URL de conexión completa para MySQL usando aiomysql.
+        """        
         if v:
             return v
 
--- app/core/db.py
+++ app/core/db.py
@@ -18,10 +18,31 @@ SessionLocal = async_sessionmaker(
 
 
 async def init_db():
+    """
+    Clase de configuración de la aplicación.
+
+    Atributos:
+        database_url (str): URL de conexión a la base de datos.
+        debug (bool): Indica si la aplicación corre en modo debug.
+        secret_key (str): Clave secreta utilizada para la seguridad.
+    """
     async with engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
 
 
 async def get_session():
+    """
+    Proporciona una sesión asíncrona de base de datos para ser utilizada en
+    endpoints o servicios.
+
+    La función actúa como una dependencia de FastAPI: abre una sesión usando
+    `SessionLocal()` dentro de un contexto asíncrono y la va entregando con
+    `yield`, permitiendo que FastAPI gestione correctamente su ciclo de vida.
+    Una vez completada la operación del endpoint, la sesión se cierra
+    automáticamente.
+
+    Yields:
+        AsyncSession: Sesión de base de datos lista para ejecutar consultas.
+    """
     async with SessionLocal() as session:
         yield session
